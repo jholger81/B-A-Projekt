1461d55847112c06051e64e5ed202996070aa122
Holger Jüptner - creativITy GmbH <holger.jueptner@creativity-gmbh.de>
2023-12-18 13:52:38 +0100

    fix swap tables

--- B&A Projekt/B&A-Software/B&A-Software/TableSwap.cs
+++ B&A Projekt/B&A-Software/B&A-Software/TableSwap.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using B_A_Software.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.OleDb;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,145 +14,188 @@ namespace B_A_Software
 {
     public partial class TableSwap : Form
     {
-        public TableSwap()
+        string strTemp = @$"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={Directory.GetCurrentDirectory()}\B&A_DB.accdb";
+        int toTable = 0;
+        int fromTable_;
+        List<MenüItem> Warenkorb;
+        public TableSwap(int fromTable, List<MenüItem> Waren)
         {
+            fromTable_ = fromTable;
+            Warenkorb = Waren;
             InitializeComponent();
         }
 
         private void Swap()
         {
-            AreaSelect areaselect = new AreaSelect();
+            using (OleDbConnection dataConnection = new OleDbConnection(strTemp))
+            {
+                dataConnection.Open();
+                using (OleDbCommand dataCommand = dataConnection.CreateCommand())
+                {
+                    dataCommand.Connection = dataConnection;
+                    dataCommand.CommandText = @$"
+                        UPDATE 
+                            Bestellung 
+                        SET TischID = {toTable}
+                        WHERE 
+                            TischID = {fromTable_}
+                        AND
+                            Bedient = false;";
+                    dataCommand.ExecuteNonQuery();
+                }
+            }
 
             this.Hide();
-
-            if (areaselect.ShowDialog() == DialogResult.OK)
-            {
-
-            }
+            new TableView(toTable, Warenkorb).ShowDialog();
         }
 
         private void TableSwapBtn1_Click(object sender, EventArgs e)
         {
+            toTable = 1;
             Swap();
         }
 
         private void TableSwapBtn2_Click(object sender, EventArgs e)
         {
+            toTable = 2;
             Swap();
         }
 
         private void TableSwapBtn3_Click(object sender, EventArgs e)
         {
+            toTable = 3;
             Swap();
         }
 
         private void TableSwapBtn4_Click(object sender, EventArgs e)
         {
+            toTable = 4;
             Swap();
         }
 
         private void TableSwapBtn5_Click(object sender, EventArgs e)
         {
+            toTable = 5;
             Swap();
         }
 
         private void TableSwapBtn6_Click(object sender, EventArgs e)
         {
+            toTable = 6;
             Swap();
         }
 
         private void TableSwapBtn7_Click(object sender, EventArgs e)
         {
+            toTable = 7;
             Swap();
         }
         private void TableSwapBtn8_Click(object sender, EventArgs e)
         {
+            toTable = 8;
             Swap();
         }
 
         private void TableSwapBtn9_Click(object sender, EventArgs e)
         {
+            toTable = 9;
             Swap();
         }
 
 
         private void TableSwapBtn10_Click(object sender, EventArgs e)
         {
+            toTable = 10;
             Swap();
         }
         private void TableSwapBtn11_Click(object sender, EventArgs e)
         {
+            toTable = 11;
             Swap();
         }
 
         private void TableSwapBtn12_Click(object sender, EventArgs e)
         {
+            toTable = 12;
             Swap();
         }
 
         private void TableSwapBtn13_Click(object sender, EventArgs e)
         {
+            toTable = 13;
             Swap();
         }
 
         private void TableSwapBtn14_Click(object sender, EventArgs e)
         {
+            toTable = 14;
             Swap();
         }
 
         private void TableSwapBtn15_Click(object sender, EventArgs e)
         {
+            toTable = 15;
             Swap();
         }
 
         private void TableSwapBtn16_Click(object sender, EventArgs e)
         {
+            toTable = 16;
             Swap();
         }
 
         private void TableSwapBtn17_Click(object sender, EventArgs e)
         {
+            toTable = 17;
             Swap();
         }
 
         private void TableSwapBtn18_Click(object sender, EventArgs e)
         {
+            toTable = 18;
             Swap();
         }
 
         private void TableSwapBtn19_Click(object sender, EventArgs e)
         {
+            toTable = 19;
             Swap();
         }
 
         private void TableSwapBtn20_Click(object sender, EventArgs e)
         {
+            toTable = 20;
             Swap();
         }
 
         private void TableSwapBtn21_Click(object sender, EventArgs e)
         {
+            toTable = 21;
             Swap();
         }
 
         private void TableSwapBtn22_Click(object sender, EventArgs e)
         {
+            toTable = 22;
             Swap();
         }
 
 
         private void TableSwapBtn23_Click(object sender, EventArgs e)
         {
+            toTable = 23;
             Swap();
         }
 
         private void TableSwapBtn24_Click(object sender, EventArgs e)
         {
+            toTable = 24;
             Swap();
         }
 
         private void TableSwapBtn25_Click(object sender, EventArgs e)
         {
+            toTable = 25;
             Swap();
         }
     }
